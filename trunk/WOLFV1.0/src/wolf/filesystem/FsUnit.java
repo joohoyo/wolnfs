b6bc53a83889eb661e868d40b9ca2b82f2b36fd6
newnov07@naver.com <newnov07@naver.com@8fae7d9c-9159-11de-800e-5b313e0d2bf3>
2009-09-28 17:43:36 +0000

    실행순서 1. WOLF_server 프로젝트의 Server.java 실행 2. WOLFV1.0 안드로이드 실행 3. telnet localhost 5554 4. redir add tcp:12313:12313 5. iplist에 아이피 입력 6. fstest 누른후 대기

--- trunk/WOLFV1.0/src/wolf/filesystem/FsUnit.java
+++ trunk/WOLFV1.0/src/wolf/filesystem/FsUnit.java
@@ -12,27 +12,28 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
-import wolf.project.R;
 import wolf.project.woltest;
-import android.app.Activity;
 import android.util.Log;
-import android.widget.TextView;
 
 //public class FsUnit {
-public class FsUnit extends Activity {
+public class FsUnit {
 	private static final String tag = "FsUnit";
 
 	private static int stepNumber;
-	private static int portNumber = 12312;
+	private static int serverPortNumber = 12312;
+	private static int androidPortNumber = 12313; 
 	private static final int STEP_INIT = 0;
 	private static final int STEP_REQUEST_DIR = 1;
 	private static final int STEP_REQUEST_FILE = 3;
 	private static final int STEP_CLOSE = 5;
+
+	public InetAddress serverAddress = null;
+	public Socket receiveSocket = null;
 	
-	public InetAddress ia = null;
-	public Socket soc = null;
-	public PrintWriter out = null;
-	
+	public Socket sendSocket = null;
+
+	public BufferedReader in = null;
+
 	void step(int stepNumber) {
 		this.stepNumber = stepNumber;
 		switch(stepNumber) {
@@ -49,17 +50,17 @@ public class FsUnit extends Activity {
 			close();
 		}
 	}
-	
+
 	void init() {
 		//woltest.get_selec_IP <- 서버의 ip정보
-		
+
 		//주소 확보
 		try {
-			ia = InetAddress.getByName(woltest.get_selec_IP);
+			serverAddress = InetAddress.getByName(woltest.get_selec_IP);
 		} catch (UnknownHostException e2) {
 			// do nothing
 		}
-		
+		//서버확인
 		int iteration = 1;
 		while(true) {
 			if (isPortNumber(iteration)) break;
@@ -70,14 +71,10 @@ public class FsUnit extends Activity {
 			}
 			iteration++;
 		}
-		try {
-			out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(soc.getOutputStream())));
-		} catch (IOException e) {
-			// do nothing
-		}		
+		
 		//여기까지 오면 전송 가능		
 	}
-	
+
 	boolean isPortNumber(int iteration) {
 		//필요한지는 모르겠지만 서버에서 기본포트넘버(12312)로 실패하였을경우
 		//차례대로 증가 시키면서 검색한다. 1, 12, 123 이런식으로		
@@ -85,58 +82,86 @@ public class FsUnit extends Activity {
 		{
 			boolean checkServer = true;
 			try {			
-				soc = new Socket(ia, portNumber+i);
+				sendSocket = new Socket(serverAddress, serverPortNumber+i);
 			} catch (IOException e1) {
 				checkServer = false;
 				// 아직 서버가 준비되지 않았음
 				// 라고 뭔가 보여줘야 하지 않을까
 			}
-			if (checkServer) return true;
+			if (checkServer) {
+				serverPortNumber += i;
+				return true;
+			}
 		}		
 		return false;
 	}
-	
+
 	void requestDir() {
-		ServerSocket ss = null;
+		ServerSocket androidSocket = null;
 		Socket so = null;
 		BufferedReader in = null;
+		PrintWriter out = null;
 		try {
-			ss = new ServerSocket(portNumber);
-			out.println("1. "+FsList.dirPath+"\n");
+			out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(sendSocket.getOutputStream())));
+
+			out.println("1. "+FsList.dirPath+"\n"); 
 			out.flush();
 
-			Log.d(tag,"println(1.dirpath)");
-			so = ss.accept();
+			//androidPortNumber = sendSocket.getLocalPort();
+			sendSocket.close();
+			Log.d(tag,"println(1.dirpath) portNumber:"+androidPortNumber);
+			
+			androidSocket = new ServerSocket(androidPortNumber);
+			androidSocket.setSoTimeout(5000);
+
+			Log.d(tag,"println(1.dirpath) portNumber:"+androidPortNumber);
+			so = androidSocket.accept();
 			Log.d(tag,"accept");
 			in = new BufferedReader(new InputStreamReader(so.getInputStream()));	
+
+			//TextView t = (TextView) Activity.findViewById(R.id.FS_TextView_DIR);
 			
-			TextView t = (TextView) findViewById(R.id.FS_TextView_DIR);
 			String strAll = null;
+			/*
 			while(true) {
+			
 				String str = in.readLine();
 				if (str.equals(new String("\n"))) {
 					break;
 				}
 				strAll += str;				
 			}
-			t.setText(strAll);
+			 */
+			int debugi=0;
 			
+			while(true) {
+				
+				debugi++;
+				
+				String str = in.readLine();
+				Log.d(tag,debugi + str);
+				if (str == null) {
+					break;
+				}
+				FsList.fsList = FsList.fsList + str + "\n";				
+			}
+			so.close();
 			
 		} catch(IOException e) {
+			Log.d(tag, "what'up");
 			//do nothing
 		}
 		
 		
 		
-		
 	}
-	
+
 	void requestFile() {
-		
+
 	}
-	
+
 	void close() {
-		out.close();
+		//out.close();
 	}
 	/*
 	 * 0. 초기화(tcp 설정)
